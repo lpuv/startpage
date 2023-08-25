12e9f6d92a9cf0ecd1c98b84d2fd4d9d4b012619
Leo Puvilland <leo@craftcat.dev>
2023-08-25 08:52:40 -0700

    change search engine

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ searchEngines = {
     "Qwant": "https://www.qwant.com/?q=",
     "Baidu": "https://www.baidu.com/s?wd=",
     "Brave": "https://search.brave.com/search?q=",
-    "Whoogle": "https://search.leokub.top/search?q="
+    "Whoogle": "https://search.craftcat.dev/search?q="
 }
 validWeatherUnit = [
     "fah", "cel"
